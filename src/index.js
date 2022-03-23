47103220f0ed5eab08c24fa4c8a0722e724ed528
Camila <mkp.camila@gmail.com>
2022-03-23 15:07:45 +0100

    📝 update books Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const usersRouter = require('./routes/users.js');
 const booksRouter = require('./routes/books.js');
@@ -12,12 +13,13 @@ const port = process.env.PORT || 3000;
 const databaseURL = process.env.DB_URL+'/'+process.env.DB_NAME;
 
 const db = mongoose.connect(databaseURL)
-    .then(() => console.log('🗄️ database connected🗄️'))
+    .then(() => console.log('🗄️ database connected 🗄️'))
     .catch((err) => console.log('📢 Oops, something went wrong 📢', err.reason));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cookieParser());
 app.use(setCors);
 
 // routes
