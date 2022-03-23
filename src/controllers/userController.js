5869c88a1326591d92be128e72ba25905c7fd59b
Camila <mkp.camila@gmail.com>
2022-03-23 01:40:01 +0100

    📝 add update user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { validationResult } = require('express-validator');
 const validator = require('express-validator');
 const jwt = require('jsonwebtoken');
 const User = require('../Models/User.js');
@@ -76,17 +77,49 @@ exports.oneUser = (req, res) =>
     if(!user)throw new Error('not found');
     res.status(200).json({
         success: true,
-        data: user,
+        data: user
     });
 };
 
-// POST :ID
+// PUT :ID
 exports.update = (req, res) =>
 {
+    const { id } =req.params;
+    const { password } = req.body;
 
+    if(id !== req.tokenUser)
+    {
+        return res.status(400).json({
+            success: false,
+            message: 'you are not authorized to proceed'
+        });
+    }
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    if(password)
+    {
+        const updatedPassword = User.find({ _id: id });
+        updatedPassword.password = crypto.createHmac('sha256', process.env.SECRET_TOKEN).update(password).digest('hex');
+
+        User.findOneAndUpdate({ _id: id }, { ...req.body, password: updatedPassword.password });
+        return res.status(200).json({
+            success: true,
+            message: 'User updated'
+        });
+    }
+    const user = User.findOneAndUpdate({ _id: id }, { ...req.body });
+    res.status(200).json({
+        success: true,
+        message: 'User updated'
+    });
+
+    if(!user) throw new Error('user not found!')
 };
-
-// PUT :ID
+// DELETE :ID
 
 // POST
 exports.login = (req, res) =>
