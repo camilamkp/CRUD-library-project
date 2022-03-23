efec823af8c277e2a8a34e31de41c0e64407bb66
Camila <mkp.camila@gmail.com>
2022-03-23 02:03:47 +0100

    add Book model

---
+++ src/Models/Book.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const imageSchema = new Schema({
+    name: String,
+    data: Buffer,
+    contentType: String,
+}
+,
+{ _id : false });
+
+const bookSchema = new Schema({
+    ISBN: Number,
+    title: String,
+    author: String,
+    originalSprache: String,
+    isAvailable: Boolean,
+    genres: {
+        type: String, 
+        enum: [ 'Action', 'Drama', 'History', 'Romance', 'Sci-Fi', 'Adventure', 'Crime', 'Mystery' ]
+    },
+    year: Number,
+    tags: [ Array ],
+    pages: Number,
+    img: imageSchema
+
+},
+{ timestamps: true });
+
+const bookModel = new Model('Book', bookSchema, 'books');
+
+module.exports = bookModel;
